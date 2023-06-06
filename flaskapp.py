41fdd330430b0e04280be6cb5080fe90a88c2611
Seher Kanwal <92606737+Seher-Kanwal@users.noreply.github.com>
2023-06-07 00:28:14 +0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -18,24 +18,7 @@ def query(payload):
 
 @app.route('/Scoring/<string:x>')
 def hell(x):
-    API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-xlm-roberta-base-sentiment"
-    headers = {"Authorization": "Bearer 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"}
-
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json=payload)
-        return response.json()
-
-    # x = "I like you. I love you"
-    output = query({
-        "inputs": x, })
-    sorted_response = sorted(output[0], key=lambda x: x['score'], reverse=True)
-    highest_label = sorted_response[0]['label']
-    if highest_label == 'positive':
-        return str(2)
-    if highest_label == 'neutral':
-        return str(1)
-    else:
-        return str(0)
+   return str(2)
 
 
 if __name__ == '__main__':
